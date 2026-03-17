0c406d6c418279a0aaa3413ccb995378bc7b1fd3
Bhargav Balinge <96978449+bhargavbalinge@users.noreply.github.com>
2026-03-17 12:14:43 +0530

    feat: Implement Incrementer component

---
+++ Incrementer.jsx
@@ -0,0 +1,15 @@
+function Incrementer() {
+  const [number, setNumber] = React.useState(0);
+
+  const increment = () => {
+    setNumber(number + 1);
+  };
+
+  return (
+    <div>
+      <h1>Number Incrementer</h1>
+      <p>{number}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
